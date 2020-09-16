d6ad07ffc9e965043b27b7144a5837a33cd52fee
Richard Zejak <Richardzejak@hotmail.com>
2020-09-16 19:49:08 +0200

    Created Keyboard txt and class with constructor

--- Inlamningsuppgift-2/Program.cs
+++ Inlamningsuppgift-2/Program.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Inlamningsuppgift_2;
+using System;
 
 class Program
 {
     static void Main()
-    { 
+    {
+        Keyboard keyboard = new Keyboard(); //creating keyboard object from class and prints it from the constructor
+        
 
+        Console.WriteLine("");
         while (true)
         {
             
