f6fbc30b7722484d9ea63e2bbbfd6dc649b79132
Calphonar <Richardzejak@hotmail.com>
2020-09-17 14:48:27 +0200

    Added CheckWhichKey class to make main code shorter

---
+++ Inlamningsuppgift-2/CheckWhichKey.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inlamningsuppgift_2
+{
+    public class CheckWhichKey
+    {
+        public void CheckWhichKeyMethod()
+        { 
+        KeyboardPress keyboardPress = new KeyboardPress();
+        ConsoleKeyInfo key = Console.ReadKey(); // creating key variable thats equal to key pressed
+
+            switch (key.Key) // checks key press and sends key and coords to keyboardpress class
+         {// 106 buttons
+            case (ConsoleKey.W):
+                keyboardPress.ChangeTextColor("W", 24, 9, 100);
+                break;
+            case (ConsoleKey.A):
+                 keyboardPress.ChangeTextColor("A", 19, 11, 150);
+                 break;
+            case (ConsoleKey.S):
+                 keyboardPress.ChangeTextColor("S", 25, 11, 200);
+                 break;
+            case (ConsoleKey.D):
+                 keyboardPress.ChangeTextColor("D", 31, 11, 250);
+                 break;
+            }
+        }
+    }
+}
--- Inlamningsuppgift-2/Program.cs
+++ Inlamningsuppgift-2/Program.cs
@@ -11,35 +11,25 @@ class Program
 
         Keyboard keyboard = new Keyboard(); //creating keyboard object from class and prints it from the constructor
 
-        KeyboardPress keyboardPress = new KeyboardPress();
-
+        CheckWhichKey checkWhichKey = new CheckWhichKey();
         //ConsoleKeyInfo key = Console.ReadKey(); // creating key variable thats equal to key pressed
-        int x = 0;
-        int y = 0;
-        char letter = ' ';
+        int x;
+        int y;
+        char letter;
+
+
+
 
         Console.WriteLine("");
         while (true) // infinite loop
         {
-            ConsoleKeyInfo key = Console.ReadKey(); // creating key variable thats equal to key pressed
+            letter = ' ';
+            x = 0;
+            y = 0;
             Console.SetCursorPosition(x, y);
             Console.Write(letter);
 
-            switch (key.Key) // checks key press and sends key and coords to keyboardpress class
-            {
-                case (ConsoleKey.W):
-                    keyboardPress.ChangeTextColor("W", 24, 9, 100);
-                    break;
-                case (ConsoleKey.A):
-                    keyboardPress.ChangeTextColor("A", 19, 11, 150);
-                    break;
-                case (ConsoleKey.S):
-                    keyboardPress.ChangeTextColor("S", 25, 11, 200);
-                    break;
-                case (ConsoleKey.D):
-                    keyboardPress.ChangeTextColor("D", 31, 11, 250);
-                    break;
-            }
+            checkWhichKey.CheckWhichKeyMethod();
         }
     }
 }
