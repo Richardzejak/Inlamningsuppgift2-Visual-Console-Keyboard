67195bbcd7be977ae18df7b67bb66a6e2369ccb1
Calphonar <Richardzejak@hotmail.com>
2020-09-17 09:57:22 +0200

    Added if input

--- Inlamningsuppgift-2/Program.cs
+++ Inlamningsuppgift-2/Program.cs
@@ -8,12 +8,19 @@ class Program
         Console.SetWindowSize(150, 20); // Resize console window
 
         Keyboard keyboard = new Keyboard(); //creating keyboard object from class and prints it from the constructor
-        
+
+
+
 
         Console.WriteLine("");
         while (true)
         {
-            
+            ConsoleKeyInfo key = Console.ReadKey();
+            if (key.Key == ConsoleKey.A)
+            {
+
+            }
+
         }
 
 
