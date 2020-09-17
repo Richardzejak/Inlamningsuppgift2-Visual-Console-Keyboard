6d6e6841da1af8d9decf6fb60525f3543464489c
Calphonar <Richardzejak@hotmail.com>
2020-09-17 11:44:50 +0200

    Fixed buttons to go back to white when not pressing them

--- Inlamningsuppgift-2/Program.cs
+++ Inlamningsuppgift-2/Program.cs
@@ -10,36 +10,66 @@ class Program
 
         Keyboard keyboard = new Keyboard(); //creating keyboard object from class and prints it from the constructor
 
-        ConsoleKeyInfo key = Console.ReadKey(); // creating key variable thats equal to key pressed
-
+        //ConsoleKeyInfo key = Console.ReadKey(); // creating key variable thats equal to key pressed
+        int x = 0;
+        int y = 0;
+        char letter = ' ';
 
         Console.WriteLine("");
         while (true) // infinite loop
         {
+            ConsoleKeyInfo key = Console.ReadKey(); // creating key variable thats equal to key pressed
+            Console.SetCursorPosition(x, y);
+            Console.Write(letter);
+
             Console.ForegroundColor = ConsoleColor.Green;
+
 
             if (key.Key == ConsoleKey.W)
             {
-                Console.SetCursorPosition(24,9);
-                Console.Write('W');
+                x = 24; y = 9;
+                letter = 'W';
+                Console.SetCursorPosition(x,y);
+                Console.Write(letter);
+                Console.Beep(100, 150);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.SetCursorPosition(x, y);
+                Console.Write(letter);
             }
             if (key.Key == ConsoleKey.A)
             {
-
+                x = 19; y = 11;
+                letter = 'A';
+                Console.SetCursorPosition(x, y);
+                Console.Write(letter);
+                Console.Beep(150, 150);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.SetCursorPosition(x, y);
+                Console.Write(letter);
             }
             if (key.Key == ConsoleKey.S)
             {
-
+                x = 25; y = 11;
+                letter = 'S';
+                Console.SetCursorPosition(x, y);
+                Console.Write(letter);
+                Console.Beep(200, 150);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.SetCursorPosition(x, y);
+                Console.Write(letter);
             }
             if (key.Key == ConsoleKey.D)
             {
-
+                x = 31; y = 11;
+                letter = 'D';
+                Console.SetCursorPosition(x, y);
+                Console.Write(letter);
+                Console.Beep(250, 150);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.SetCursorPosition(x, y);
+                Console.Write(letter);
             }
-            if (key.Key == ConsoleKey.E)
-            {
-
-            }
-            Console.ResetColor();
+            Console.SetCursorPosition(x, y);
         }
     }
 }
