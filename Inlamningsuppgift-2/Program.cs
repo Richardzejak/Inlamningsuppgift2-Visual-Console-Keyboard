f91144f956cfa7e98b9ed7180fbb3ca11d5d48a8
Calphonar <Richardzejak@hotmail.com>
2020-09-24 11:12:08 +0200

    added try catch for exeption handling in main menu

--- Inlamningsuppgift-2/Program.cs
+++ Inlamningsuppgift-2/Program.cs
@@ -22,31 +22,44 @@ and also Beep some different kind of sounds depending on which button you press,
 Write ""1"" to start use the keyboard
 Write ""2"" to play a random tone of beeps.
 ");
-            menuChoice = Convert.ToInt32(Console.ReadLine());
-
-            if (menuChoice == 1)
+            try
             {
-                Console.Clear();
-                Console.WriteLine(Keyboard.keyboardString); //draws keyboard on construction
+                menuChoice = Convert.ToInt32(Console.ReadLine());
 
-                Console.WriteLine("");
-                while (true) // infinite loop
+
+                if (menuChoice == 1)
                 {
-                    letter = ' ';
-                    x = 0;
-                    y = 0;
-                    Console.SetCursorPosition(x, y);
-                    Console.Write(letter);
+                    Console.Clear();
+                    Console.WriteLine(Keyboard.keyboardString); //draws keyboard on construction
 
-                    CheckWhichKey.CheckWhichKeyMethod();
+                    Console.WriteLine("");
+                    while (true) // infinite loop
+                    {
+                        letter = ' ';
+                        x = 0;
+                        y = 0;
+                        Console.SetCursorPosition(x, y);
+                        Console.Write(letter);
+
+                        CheckWhichKey.CheckWhichKeyMethod();
+                    }
+                }
+                else if (menuChoice == 2)
+                {
+                    RandomTones.RandomTonePlayer();
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Input number was not in correct format please try again with either 1 or 2 \n");
                 }
             }
-            if (menuChoice == 2)
+            catch (Exception e)
             {
-                RandomTones.RandomTonePlayer();
                 Console.Clear();
+                Console.WriteLine("Input number was not in correct format please try again with either 1 or 2 \n");
             }
-
         }
 
 
